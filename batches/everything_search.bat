066980f215c7d8c8aa6071862c3552bbe2020ab1
44H4 <raphael.gamba7@outlook.com>
2024-12-28 15:14:45 +0800

    D

---
+++ batches/everything_search.bat
@@ -0,0 +1,39 @@
+@echo off
+color a
+:: Check for administrator privileges
+net session >nul 2>&1
+if %ERRORLEVEL% NEQ 0 (
+    echo This script requires administrative privileges.
+    pause
+    exit /b
+)
+
+REM Check if winget is available
+echo --------------------------------------------------------
+echo        Winget Hub Installer - Everything Search
+echo --------------------------------------------------------
+echo Made by : 44H4
+echo Opening the WinGet Hub website (minimized) for more information...
+
+pause
+echo Checking for WinGet...
+winget --version >nul 2>&1
+if %ERRORLEVEL% NEQ 0 (
+    echo Winget is not installed or not available on your system.
+    echo Please install the App Installer from the Microsoft Store and try again.
+    pause
+    exit /b
+)
+
+REM Installing Everything Search
+echo Installing Everything Search...
+winget install -e --id voidtools.Everything
+echo Opening the WinGet Hub website (minimized) for more information...
+start /MIN powershell -command "Start-Process 'https://winget-hub.pages.dev'"
+REM Check installation status
+if %ERRORLEVEL% EQU 0 (
+    echo Everything Search installed successfully.
+) else (
+    echo Failed to install Everything Search. Please check for errors above.
+)
+pause
